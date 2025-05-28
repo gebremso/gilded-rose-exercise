3ed942ce81e721de5244214578adabf4001102ad
Solomon G <gebremso@gmail.com>
2025-05-28 08:20:25 +0100

    Product representation and JUnit test

---
+++ src/test/java/com/solo/gilded/rose/inventory/ProductTest.java
@@ -0,0 +1,67 @@
+package com.solo.gilded.rose.inventory;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    //Z -Zero when input date is the same as purchased date
+    @Test
+    public void testDatePassZero(){
+        LocalDate date = LocalDate.of(2025,5,25);
+        Product product = new Product("Apple", LocalDate.of(2025,5,25));
+        assertEquals(0, product.daysPassed(date));
+    }
+
+    //O -One  1 day before purchasing date
+    @Test
+    public void testOneDayAfterPurchaseDate(){
+        LocalDate date = LocalDate.of(2025, 5, 26);
+        Product product = new Product("Samsung", LocalDate.of(2025,5, 25));
+        assertEquals(1, product.daysPassed(date));
+    }
+
+    //M - Many many days pass the purchasing date
+    @Test
+    public void testManyDayPassPurchaseDate(){
+        LocalDate date = LocalDate.of(2025, 5, 26);
+        Product product = new Product("Samsung", LocalDate.of(2025,5, 20));
+        assertEquals(6, product.daysPassed(date));
+    }
+
+    //B - Boundary after year has passed
+    @Test
+    public void testYearPassedPurchaseDate(){
+        LocalDate date = LocalDate.of(2025,5, 29);
+        Product product = new Product("Ipad", LocalDate.of(2024,5, 27));
+        assertEquals(367,product.daysPassed(date));
+    }
+
+    //I - Interface valid interaction with the method
+    @Test
+    public void testCoupleOfDaysPassPurchaseDate(){
+        Product product = new Product("Phone");
+        assertEquals(3, product.daysPassed(LocalDate.now().plusDays(3)));
+    }
+
+    //E - Exception validate when the input is null
+    @Test
+    public void testNullInputForDate(){
+        Product product = new Product("Phone");
+        assertThrows(IllegalArgumentException.class, ()->product.daysPassed(null));
+
+        Product product2 = new Product("Tablet", LocalDate.of(2025,5,26));
+        assertThrows(IllegalArgumentException.class, ()->product.daysPassed(LocalDate.of(2025,5,25)));
+    }
+
+    //E - Exception validate when the input is before the purchase date
+    @Test
+    public void testInputBeforePurchaseDate(){
+        Product product = new Product("Tablet", LocalDate.of(2025,5,26));
+        assertThrows(IllegalArgumentException.class, ()->product.daysPassed(LocalDate.of(2025,5,25)));
+    }
+
+}
